559874039b6572f8dfe89f98e6f80fa823f28369
PoopFace <danilsteam-88@hotmail.com>
2023-07-25 03:23:44 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,166 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <img src="/assets/logo.svg" />
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Shop</li>
+            <li>Pages</li>
+          </ul>
+        </nav>
+        <div className="navbar-options">
+          <div className="navbar-cart-icon-container">
+            <img src="/assets/cart.svg" />
+            <div className="navbar-cart-counter">0</div>
+          </div>
+          <button>Contact Us</button>
+        </div>
+      </header>
+      <main>
+        <div className="fragment-1">
+          <div className="inner-fragment-container">
+            <div className="fragment-1-links">
+              <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Twitch</li>
+              </ul>
+            </div>
+            <div className="fragment-1-info-and-img-container">
+              <div className="fragment-1-info">
+                <div className="future-of-esports-container">
+                  <img src="/assets/flame.svg" />
+                  <div>FUTURE OF eSPORTS</div>
+                </div>
+                <h2 className="fragment-1-heading">
+                  Unleash the Next <span>Generation</span> of Gaming
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Felis, nec donec in morbi pulvinar. Enim non pulvinar neque.
+                </p>
+                <div className="fragment-1-options">
+                  <button>Explore More</button>
+                  <div>View our team</div>
+                </div>
+              </div>
+              <img src="/assets/fr1img.png" />
+            </div>
+          </div>
+        </div>
+        <div className="fragment-2">
+          <div className="inner-fragment-container">
+            <img src="/assets/fr2img.png" />
+            <div className="fragment-2-info">
+              <div className="future-of-esports-container">
+                <img src="/assets/flame.svg" />
+                <div>FUTURE OF eSPORTS</div>
+              </div>
+              <h2 className="fragment-2-heading">
+                Customize your own character
+              </h2>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae.
+              </p>
+              <div className="fragment-2-imgs-container">
+                <img src="/assets/fr2img21.png" />
+                <img src="/assets/fr2img22.png" />
+                <img src="/assets/fr2img23.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="fragment-3">
+          <div className="inner-fragment-container">
+            <div className="future-of-esports-container">
+              <img src="/assets/flame.svg" />
+              <div>FUTURE OF eSPORTS</div>
+            </div>
+            <div className="fragment-3-heading-and-button-container">
+              <h2 className="fragment-3-heading">Latest News & articles</h2>
+              <button>Read More</button>
+            </div>
+            <div className="fragment-3-articles-container">
+              <article className="fragment-3-big-article">
+                <img src="/assets/fr3img1.png" />
+                <div className="fragment-3-article-date">April 2, 2021</div>
+                <h3 className="fragment-3-article-heading">
+                  Esports Group teams up with the Indianapolis Colts
+                </h3>
+              </article>
+              <div className="fragment-3-small-articles-subcontainer">
+                <article className="fragment-3-small-article">
+                  <img src="/assets/fr3img2.png" />
+                  <div className="fragment-3-article-date">April 2, 2021</div>
+                  <h3 className="fragment-3-article-heading">
+                    NAVI reveals s1mple fifth anniversary
+                  </h3>
+                </article>
+                <article className="fragment-3-small-article">
+                  <img src="/assets/fr3img3.png" />
+                  <div className="fragment-3-article-date">April 2, 2021</div>
+                  <h3 className="fragment-3-article-heading">
+                    A1esports Shares new picture
+                  </h3>
+                </article>
+                <article className="fragment-3-small-article">
+                  <img src="/assets/fr3img4.png" />
+                  <div className="fragment-3-article-date">April 2, 2021</div>
+                  <h3 className="fragment-3-article-heading">
+                    T1 unveil partnership with Razer
+                  </h3>
+                </article>
+                <article className="fragment-3-small-article">
+                  <img src="/assets/fr3img5.png" />
+                  <div className="fragment-3-article-date">April 2, 2021</div>
+                  <h3 className="fragment-3-article-heading">
+                    RX secures content partnership with
+                  </h3>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div className="footer-info-container">
+          <img src="/assets/logo.svg" />
+          <p>
+            Lorem ipsum dolor amet, consectetur adipiscing elit. Eget nisl nunc
+            quam ac sed turpis volutpat. Cursus sed massa non .
+          </p>
+        </div>
+        <div className="footer-info-container">
+          <div>Menu Items</div>
+          <ul>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Shop</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="footer-info-container">
+          <div>Other Pages</div>
+          <ul>
+            <li>Styleguide</li>
+            <li>Changelog</li>
+            <li>Licenses</li>
+            <li>Team</li>
+          </ul>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
